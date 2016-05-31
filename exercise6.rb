61566c0be26efdc2e3470b984b7cec2cdd26e792
Keira Rochester <keira@keira.local>
2016-05-31 18:46:03 -0400

    Select second item in array, and Sort list alphabetical order with asterisk

--- exercise6.rb
+++ exercise6.rb
@@ -13,5 +13,19 @@ grocery_list.push("rice")
 #Reprints the grocery_list, including "rice"
 print_list(grocery_list)
 
-
+#Counts number of items on list
 puts grocery_list.length
+
+#Check if the grocery_list includes bananas?
+if grocery_list.include?("bananas")
+  puts "You don't need to pick up bananas today".
+else
+  puts "You need to pick up bananas"
+end
+#Display the second item in the list
+puts grocery_list[1]
+
+#Sort list in aphabetical order, with asterisk
+grocery_list.sort.each do |item|
+  puts "*#{item}"
+end
