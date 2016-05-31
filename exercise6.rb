3ab0aecdb839f04288288b9e226b9d41e9c10a0f
Keira Rochester <keira@keira.local>
2016-05-31 18:24:06 -0400

    Grocery list with 'rice' added

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,14 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each do |item|
-  puts "*#{item}"
+def print_list(grocery_list)
+  grocery_list.each do |item|
+    puts "*#{item}"
+  end
 end
+
+#Calls the print_list method that iterates through the grocery_list array
+print_list(grocery_list)
+#Pushes "rice" to apprend the array
+grocery_list.push("rice")
+#Reprints the list, including "rice"
+print_list(grocery_list)
